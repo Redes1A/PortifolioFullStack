1c8476e6015988583ed7f56d76eb9919b0308fad
Redes1A <153667204+Redes1A@users.noreply.github.com>
2023-12-12 16:00:56 -0300

    PrimeiraAtualizaçãoPortifolioFullStack
    Trabalho Portifólio Full Stack

---
+++ portifólio/port.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector('.navbar');
+
+button.addEventListener('click', function(){
+mobileNavbar.classList.toggle('active');
+});
+window.addEventListener('scroll', function(){
+    if(this.window.pegeVOffset > 0) return navbar.classList.add('active');
+    return navbar.classList.remove('active');
+});
